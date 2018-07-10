611a4e21b449c26343badad3c2cf4310e7035ff2
eduardovalentim7 <eduardovalentim79@gmail.com>
2018-07-10 14:59:31 -0300

    Lazy Loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage } from '../../../node_modules/ionic-angular/umd';
+import { NavController } from '../../../node_modules/ionic-angular/navigation/nav-controller';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
