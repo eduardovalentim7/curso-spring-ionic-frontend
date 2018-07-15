c3a6f4a19d28fc886c06de0a62cd2da1efcd8870
eduardovalentim7 <eduardovalentim79@gmail.com>
2018-07-15 16:33:01 -0300

    Desabilitar manu tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage } from '../../../node_modules/ionic-angular/umd';
 import { NavController } from '../../../node_modules/ionic-angular/navigation/nav-controller';
+import { MenuController } from '../../../node_modules/ionic-angular/components/app/menu-controller';
 
 
 @IonicPage()
@@ -11,8 +12,15 @@ import { NavController } from '../../../node_modules/ionic-angular/navigation/na
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menu:MenuController) {
+  }
 
+  
+  ionViewWillEnter() {
+  this.menu.swipeEnable(false);
+  }
+  ionViewDidLeave() {
+  this.menu.swipeEnable(true);
   }
   login(){
     this.navCtrl.setRoot('CategoriasPage');
